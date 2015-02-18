0ee18cd6e07157b7c235085b0b60e45099fc83e1
Rayanb <rayanbouts@gmail.com>
2015-02-18 10:14:04 -0500

    auth, models, migrations

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -2,10 +2,39 @@ get '/' do
   erb :welcome
 end
 
-get '/login' do
-  
+get '/authenticate/login' do
+  erb :'auth/login'
 end
 
-get '/signup' do
-  
+post '/authenticate/login' do
+  user = User.find_by(email: params[:email])
+  if user && user.try(:authenticate, params[:password])
+    session[:user_id] = user.id
+    erb :main
+  else
+    erb :'auth/login'
+  end
+end
+
+get '/authenticate/signup' do
+  erb :'auth/signup'
+end
+
+post '/authenticate/signup' do
+  name = params[:name]
+  email = params[:email]
+  password= params[:password]
+  password_conf = params[:password_confirmation]
+  if name && email && password == password_conf
+    user = User.create(name: name, email: email, password: password, password_confirmation: password_conf)
+    session[:user_id] = user.id
+    erb :main
+  else
+    redirect '/authenticate/signup'
+  end
+end
+
+get '/logout' do
+  session.delete(:user_id)
+  erb :welcome
 end
